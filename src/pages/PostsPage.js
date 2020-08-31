3a3fcf1a427ee99ac17af25ef582559bd0d3b582
dkersten <dkersten1994@gmail.com>
2020-08-31 19:28:53 -0400

    tutorial functionailty completed

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions/postsActions'
+import { Post } from '../components/Post'
 
 const PostsPage = ({dispatch, loading, posts, hasErrors}) => {
     useEffect(() => {
         dispatch(fetchPosts())
     }, [dispatch])
 
+    const renderPosts = () => {
+        if (loading) return <p>Loading posts...</p>
+        if (hasErrors) return <p>Unable to display posts.</p>
+        return posts.map((post) => <Post key={post.id} post={post} />)
+    }
+
     return(
         <section>
             <h1>Posts</h1>
+            {renderPosts()}
         </section>
     )
 }
