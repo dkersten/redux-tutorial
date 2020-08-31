06f938e22ef5591ffb0c4a1999e59a9d01aa7744
dkersten <dkersten1994@gmail.com>
2020-08-31 19:19:07 -0400

    fetch functionality added

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions/postsActions'
 
-const PostsPage = () => {
+const PostsPage = ({dispatch, loading, posts, hasErrors}) => {
+    useEffect(() => {
+        dispatch(fetchPosts())
+    }, [dispatch])
+
     return(
         <section>
             <h1>Posts</h1>
@@ -8,4 +14,10 @@ const PostsPage = () => {
     )
 }
 
-export default PostsPage
+const mapStateToProps = (state) => ({
+    loading: state.posts.loading,
+    posts: state.posts.posts,
+    hasErrors: state.posts.hasErrors,
+})
+
+export default connect(mapStateToProps)(PostsPage)
